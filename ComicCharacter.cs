66f367715bf77a3909961f991c3522cb4c2827d3
brogowski <bartlomiej.rogowski@degustudios.com>
2019-03-03 13:57:00 +0100

    Setup simple parsing

---
+++ ComicCharacter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Sandbox
+{
+    internal class ComicCharacter
+    {
+        public int PageId { get; set; }
+        public string Name { get; set; }
+        public string Urlslug { get; set; }
+        public string Id { get; set; }
+        public string Align { get; set; }
+        public string Eye { get; set; }
+        public string Hair { get; set; }
+        public string Sex { get; set; }
+        public string Gsm { get; set; }
+        public string Alive { get; set; }
+        public string Apperances { get; set; }
+        public string FirstApperance { get; set; }
+        public int? Year { get; set; }
+
+        public override string ToString()
+        {
+            return GetType()
+                .GetProperties()
+                .Select(x => (name: x.Name, value: x.GetValue(this)))
+                .Aggregate("", (acc, x) => string.Join(Environment.NewLine, acc, $"{x.name}: {x.value}"));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Sandbox
 {
@@ -10,6 +11,36 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
+            var input = File.ReadAllLines(@"marvel-wikia-data.csv");
+            var parsed = input.Skip(1).Select(ParseCsvLine);
+
+            foreach (var item in parsed)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static ComicCharacter ParseCsvLine(string line)
+        {
+            var fields = line.Split(',');
+            return new ComicCharacter
+            {
+                PageId = int.Parse(fields[0]),
+                Name = fields[1],
+                Urlslug = fields[2],
+                Id = fields[3],
+                Align = fields[4],
+                Eye = fields[5],
+                Hair = fields[6],
+                Sex = fields[7],
+                Gsm = fields[8],
+                Alive = fields[9],
+                Apperances = fields[10],
+                FirstApperance = fields[11],
+                Year = int.TryParse(fields[12], out int result) ? (int?)result : null
+            };
         }
     }
 }
